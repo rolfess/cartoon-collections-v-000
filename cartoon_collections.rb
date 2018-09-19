ff7e03c8ece09a06e56816c9f56c3f65cb04b24d
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-19 12:20:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,11 +14,7 @@ end
 def long_planeteer_calls (array)
    less_4 = false
    less_4 = array.all? do |calls|
-    if calls.length > 3
-     return true
-    else
-     return false
-    end
+    if calls.length > 3 true
    end
 end
 
