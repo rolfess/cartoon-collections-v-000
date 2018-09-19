feb3b6712b3d9a6b3f6520223764b309a05dbcfd
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-19 11:55:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (array)
+  array.each_with_index {|item, index| puts"#{index + 1}. #{array[index]}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (array)
+  cap_planet = []
+  array.each  do|planet|
+   cap_planet << planet.capitalize + "!"
+  end
+  cap_planet
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (array)
+   array.each do |calls|
+   if calls.length > 3
+    return true
+   else
+    return false
+   end
+   end
 end
 
 def find_the_cheese# code an argument here
